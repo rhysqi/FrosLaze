41502b661efa0630f7624d2cc5690ed8f88a64af
rhysqi <rhysqi.akbar@gmail.com>
2024-01-25 05:18:16 +0700

    src: Update Rhoux-FrosLaze.c
    Adding functions bind to parse argument from software
    
    submit: rhysqi <rhysqi.akbar@gmail.com>

--- src/Rhoux-FrosLaze.c
+++ src/Rhoux-FrosLaze.c
@@ -1,9 +1,14 @@
 
 #include "../include/Rhoux-FrosLaze.h"
 
-int main(int argc, const char *argv[]){
-    
-    
+int main(int argc, char *argv[]){
+
+    Rhoux_Args(argc, argv);
+
+    Blaze_Args(argv);
+    Frost_Args(argv);
+
+    Rhoux_FrosLaze(argc,*argv, 1);
 
     return 0;
 }
