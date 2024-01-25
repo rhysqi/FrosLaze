a9e4a5a136551ba7bdaa8085d9c8d7819f45e1cb
rhysqi <rhysqi.akbar@gmail.com>
2024-01-25 16:23:26 +0700

    lib: Adding Blaze libraries
    Adding new libraries for Blaze
    
    submit: rhysqi <rhysqi.akbar@gmail.com>

---
+++ lib/Blaze-Arguments.c
@@ -0,0 +1,18 @@
+
+#include "../include/Blaze-Parser.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+char *Blaze_Args_t[] = {"func:"};
+
+// Blaze Functions
+void Blaze_Parser(char *_VALUE[]){
+
+}
+
+void Blaze_Args(char *_VALUE[]){
+    
+}
---
+++ lib/Blaze-Symbol.c
@@ -0,0 +1,13 @@
+
+#include "../include/Blaze-Parser.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+char *Blaze_Sym_t[] = {"<", ">"};
+
+void Blaze_Symbol(char *_VALUE[]){
+
+}
