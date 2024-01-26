ab0c6aa79fb5a0b9be6d9b1a381db5a69ee76e2d
rhysqi <rhysqi.akbar@gmail.com>
2024-01-26 07:39:03 +0700

    inc: Update Frost-Parser.h Adding include guard header
    submit: rhysqi <rhysqi.akbar@gmail.com>

--- include/Frost-Parser.h
+++ include/Frost-Parser.h
@@ -1,21 +1,9 @@
-
-#include <stdio.h>
-#include <stdlib.h>
+#ifndef FROST_PARSER_H
+#define FROST_PARSER_H
 
 // Declaration for Frost Functions
 void Frost_Parser(char *_VALUE[]);
 void Frost_Symbol(char *_VALUE[]);
 void Frost_Args(char *_VALUE[]);
 
-// Frost Functions
-inline void Frost_Parser(char *_VALUE[]){
-
-}
-
-inline void Frost_Symbol(char *_VALUE[]){
-
-}
-
-inline void Frost_Args(char *_VALUE[]){
-
-}
+#endif /* FROST_PARSER_H */
