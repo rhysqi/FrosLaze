0c92dd41460db36cbaf410f2c3c075f43710194f
rhysqi <rhysqi.akbar@gmail.com>
2024-01-27 12:37:58 +0700

    inc: Update Rhoux-Parser header
    Adding some functions declaration
    
    submit: rhysqi <rhysqi.akbar@gmail.com>

--- include/Rhoux-Parser.h
+++ include/Rhoux-Parser.h
@@ -5,9 +5,14 @@
 
 // Declaration for Rhoux-FrosLaze Functions
 void Rhoux_FrosLaze(int _COUNT, char *_INPUT, int STATE);
-void Rhoux_Args(int COUNT, char *_VALUE[]);
+void Rhoux_Help(int COUNT, char *_VALUE[]);
 void Rhoux_Generate(char *VALUE[]);
+
+void Rhoux_Args(int COUNT, char *_VALUE[]);
 void Rhoux_Parser(char *_VALUE[]);
 void Rhoux_Symbol(char *_VALUE[]);
+
+void Rhoux_Interrupt(int SIG_INTR);
+void Rhoux_Version(int argc, char *_VALUE[]);
 
 #endif /* RHOUX_PARSER_H */
