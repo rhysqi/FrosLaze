dc403f651ab83dcec8c95d9ed60208111f51e220
rhysqi <rhysqi.akbar@gmail.com>
2024-01-27 12:41:08 +0700

    lib: Update Rhoux lib
    Adding functions binding for Rhoux, Blaze and Frost parser
    
    submit: rhsyqi <rhysqi.akbar@gmail.com>

--- lib/Rhoux.c
+++ lib/Rhoux.c
@@ -5,19 +5,68 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
+#include <signal.h>
+
+#define MIN_COUNT (1 << 8)
+#define MAX_COUNT (MIN_COUNT * 8)
 
 // Rhoux-FrosLaze Functions
-void Rhoux_FrosLaze(int _COUNT, char *_INPUT, int STATE){
-	if (_COUNT < 2) {
-	    printf("(=) Rhoux-FrosLaze build system %s (=)\n", VERSION);
+void Rhoux_Interrupt(int SIG_INTR) {
+    exit(SIG_INTR);
+}
 
+void Rhoux_FrosLaze(int _COUNT, char *_INPUT, int STATE){
+
+	char *_BUFF = (char *) malloc(sizeof(MAX_COUNT));
+
+	if (_COUNT < 2) {
+	    printf("(=) Rhoux-FrosLaze build system %s (-)\n", VERSION);
 		volatile int MSG[] = {-1, 2, 5};
+
+		// System Service
 		while (MSG[STATE] > 0) {
+
+			printf("X= ");
+			char *Base = fgets(_INPUT, MAX_COUNT, stdin);
+
+			// Check arguments length
+			if (*Base <= sizeof(char *[MAX_COUNT])) {
+				// Process Rhoux, Blaze, and Frost running
+				if (Base != NULL) {
+					char *BUFF = Base;
+					Rhoux_Parser(&BUFF);
+
+				} else if (strcmp(Base, "^C")) {
+					// Interrupt Signal
+					Rhoux_Interrupt(1);
+				}
 			
+			} else {
+				printf("Arguments are limits for %d Characters \n", MIN_COUNT);
+				exit(1);
+			}
 		}
 	}
 }
 
-void Rhoux_Parser(char *_VALUE[]){
+// Rhoux Parsing Function
+void Rhoux_Parser(char *_VALUE[MAX_COUNT]){
 
+	char *BUFF_P = _VALUE[MAX_COUNT];
+
+	// Rhoux Parsing Function Bindings
+	Rhoux_Args(0, &BUFF_P);
+	Rhoux_Symbol(&BUFF_P);
+
+	// Rhoux Parsing Function Bindings
+	Blaze_Parser(&BUFF_P);
+	Blaze_Args(&BUFF_P);
+	Blaze_Symbol(&BUFF_P);
+
+	// Frost Parsing Function Bindings
+	Frost_Parser(&BUFF_P);
+	Frost_Args(&BUFF_P);
+	Frost_Symbol(&BUFF_P);
+
+	free(BUFF_P);
 }
